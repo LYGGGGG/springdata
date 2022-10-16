a09e325837b1aaae1cb9021012090c0c8684f04e
YG <710431075@qq.com>
2022-10-16 17:41:47 +0800

    .class

--- 02-springdata-jpa/src/main/java/com/tuling/repositories/CustomerRepository.java
+++ 02-springdata-jpa/src/main/java/com/tuling/repositories/CustomerRepository.java
@@ -6,16 +6,44 @@ package com.tuling.repositories;
 */
 
 import com.tuling.pojo.Customer;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface CustomerRepository extends PagingAndSortingRepository<Customer,Long> {
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
 
     //自定义查询
     @Query("FROM Customer where custName=:custName")
     Customer findCustomerBycustName(@Param("custName") String custName);
 
+    @Transactional
+    @Modifying
+    @Query("update Customer c set c.custName=:name where c.custId=:id")
+    int updateCustomer(@Param("id") Long id, @Param("name") String name);
 
+    @Transactional
+    @Modifying
+    @Query("delete from Customer c where c.custName=:name")
+    int deleteCustomerByName(@Param("name") String name);
+
+    //原生sql
+    @Transactional
+    @Modifying
+    @Query(value = "select * from tb_customer where cust_name=:name", nativeQuery = true)
+    List<Customer> queryCustomerByName(@Param("name") String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "select * from tb_customer where id=:id", nativeQuery = true)
+    List<Customer> queryCustomerById(@Param("id") Long id);
+
+    List<Customer> findByCustAddress(String addr);
+
+    @Transactional
+    @Modifying
+    int deleteByCustId(@Param("id")Long id);
 }
--- 02-springdata-jpa/src/test/java/com/tuling/JpqlTest.java
+++ 02-springdata-jpa/src/test/java/com/tuling/JpqlTest.java
@@ -11,9 +11,10 @@ import com.tuling.repositories.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @ContextConfiguration(classes = SpringDataJPAConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,9 +23,49 @@ public class JpqlTest {
     CustomerRepository repository;
 
     @Test
-    public void testfindById(){
+    public void testfindById() {
         Customer zzz = repository.findCustomerBycustName("zzz");
         System.out.println("======================================");
         System.out.println(zzz);
+    }
+
+    @Test
+    public void testUpdateById() {
+        repository.updateCustomer(8L, "哈哈");
+    }
+
+    @Test
+    public void testDeleteByName() {
+        repository.deleteCustomerByName("zzz");
+    }
+
+    @Test
+    public void testFind() {
+        List<Customer> customers = repository.queryCustomerByName("哈哈");
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+    }
+
+    @Test
+    public void testId() {
+        List<Customer> customers = repository.queryCustomerById(9l);
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+    }
+
+    @Test
+    public void testFindA() {
+        List<Customer> customers = repository.findByCustAddress("北京");
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+    }
+
+    @Test
+    public void testFindB() {
+        int i = repository.deleteByCustId(6l);
+        System.out.println(i);
     }
 }
